3af6b0d24172481ccda4412802abbbaf26127507
Irina <irina.yankovskaya2424@gmail.com>
2024-01-04 23:50:38 +0200

    homework 5

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <App root={root} />
-  </React.StrictMode>
+    <BrowserRouter>
+        <App root={root}/>
+    </BrowserRouter>
 );
